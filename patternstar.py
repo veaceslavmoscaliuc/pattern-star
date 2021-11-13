c4661ae8327c8ce370cd3874d5073d31cd4c1e6c
veaceslavmoscaliuc <93546875+veaceslavmoscaliuc@users.noreply.github.com>
2021-11-13 09:47:38 +0200

    Add files via upload

---
+++ patternstar.py
@@ -0,0 +1,20 @@
+#   * \n
+#   * * \n
+#   * * * \n 
+#   * * * * \n
+#   * * * * * \n
+
+
+
+
+
+
+rows = 5
+star = 1
+while star <= rows :
+    j = 1
+    while j <= star:
+        print("*", end = " ")
+        j += 1
+    print()
+    star += 1
